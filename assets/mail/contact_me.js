5f9a01abf6928a32d3af1399f9a3d490ce9e3464
vincentkumalo <142814100+vincentkumalo@users.noreply.github.com>
2023-11-14 07:07:59 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://pw07bzvy4a.execute-api.eu-west-2.amazonaws.com/default/vincentfunc",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
